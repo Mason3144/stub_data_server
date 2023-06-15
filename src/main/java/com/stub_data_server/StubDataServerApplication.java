83de2167b4871b0900a6c339e16d4f950ae909f9
Mason3144 <lts890303@gmail.com>
2023-06-15 19:10:12 +0900

    init commit

---
+++ src/main/java/com/stub_data_server/StubDataServerApplication.java
@@ -0,0 +1,13 @@
+package com.stub_data_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StubDataServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StubDataServerApplication.class, args);
+	}
+
+}
