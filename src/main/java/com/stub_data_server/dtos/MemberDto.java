7d94721b86f25a600748616fcf7835eb47f0542e
Mason3144 <lts890303@gmail.com>
2023-10-14 14:43:10 +0900

    [refactor] 패키지 및 클래스 구조 및 depth 변경

---
+++ src/main/java/com/stub_data_server/dtos/MemberDto.java
@@ -0,0 +1,23 @@
+package com.stub_data_server.member;
+
+
+import lombok.Builder;
+import lombok.Getter;
+
+
+public class MemberDto {
+    @Builder
+    @Getter
+    public static class Response{
+        private long memberId;
+        private String nickname;
+    }
+    @Builder
+    @Getter
+    public static class Request{
+        private String email;
+        private String password;
+        private String nickname;
+    }
+
+}
